faed90d560171855f4247b694a435e4a9cbfa9b7
Hensi Kavar <115384276+hensikavar@users.noreply.github.com>
2023-07-09 20:39:06 +0530

    Java program to check given character is vowel or not.

---
+++ Vowel.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Vowel
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+
+		System.out.print("Enter the character : ");
+		char ch=sc.next().charAt(0);
+
+		if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+		{
+			System.out.println("The character is vowel.");
+		}
+		else
+		{
+			System.out.println("The character is not vowel.");
+		}
+	}
+}
