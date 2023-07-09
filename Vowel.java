5b591c34d15d8dca1a8c8f39a924f0722cc7cade
Hensi Kavar <115384276+hensikavar@users.noreply.github.com>
2023-07-09 20:39:54 +0530

    Update Vowel.java

--- Vowel.java
+++ Vowel.java
@@ -7,9 +7,9 @@ public class Vowel
 		Scanner sc=new Scanner(System.in);
 
 		System.out.print("Enter the character : ");
-		char ch=sc.next().charAt(0);
+		char ch=sc.next().toLowerCase().charAt(0);
 
-		if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+		if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
 		{
 			System.out.println("The character is vowel.");
 		}
