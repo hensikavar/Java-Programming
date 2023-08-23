995a6edcd9ab1e90d2eeeb8bd835a7a25c988332
Hensi Kavar <115384276+hensikavar@users.noreply.github.com>
2023-08-23 22:45:03 +0530

    Java program to find sum of first 10 natural numbers using For loop.

---
+++ Sumoffirstten.java
@@ -0,0 +1,14 @@
+class Sumoffirstten
+{
+	public static void main(String[] args)
+	{
+		int i,sum=0;
+
+		for(i=1;i<=10;i++)
+		{
+			sum=sum+i;
+		}
+		
+		System.out.print("Sum will be : "+sum);
+	}
+}
