b1b4569f075a2852fc05b6f2e28e101f0df6e857
Hensi Kavar <115384276+hensikavar@users.noreply.github.com>
2023-08-23 22:45:39 +0530

    Java program to find sum of two numbers given by user.

---
+++ SumOfTwoNumbers.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class SumOfTwoNumbers
+{
+	public static void main(String[] args)
+	{
+		int a,b;
+		Scanner sc=new Scanner(System.in);
+
+		System.out.print("Enter first number : ");
+		a=sc.nextInt();
+		System.out.print("Enter second number : ");
+		b=sc.nextInt();
+		
+		System.out.print("Difference will be : "+(a+b));
+	}	
+}
